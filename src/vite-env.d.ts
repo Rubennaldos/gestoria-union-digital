de2f73d755dff9365642dc8c10ff254b59e4f12d
Rubennaldos <albertonaldos@gmail.com>
2025-09-06 21:32:15 -0500

    cambios 7.5

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,23 +1,2 @@
 /// <reference types="vite/client" />
-
-// Fix for Worker type used by Vite
-declare global {
-  interface Worker extends EventTarget {
-    onerror: ((this: AbstractWorker, ev: ErrorEvent) => any) | null;
-    onmessage: ((this: Worker, ev: MessageEvent) => any) | null;
-    onmessageerror: ((this: Worker, ev: MessageEvent) => any) | null;
-    postMessage(message: any, transfer?: Transferable[]): void;
-    terminate(): void;
-  }
-  
-  var Worker: {
-    prototype: Worker;
-    new(scriptURL: string | URL, options?: WorkerOptions): Worker;
-  };
-  
-  interface WorkerOptions {
-    credentials?: RequestCredentials;
-    name?: string;
-    type?: "classic" | "module";
-  }
-}
+/// <reference lib="webworker" />
